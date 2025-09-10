a8373b3e5b41a26900f2ceb0ca1aae7b1e2b837d
S-Ishwarya <ish7joseph@gmail.com>
2025-09-10 22:04:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3"{
     bucket                 = "john-bucket12345"
     region                 = "eu-north-1"
-    key                    = "backend.tfstate"
+    key                    = "backend.tfstate" #The file path/name where Terraform will store its state file in the bucket
   }
 }
